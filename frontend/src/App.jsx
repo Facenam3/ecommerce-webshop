3b2e53548c8356fe0f5119c7d2635a847cce0162
Dalibor Jovanovski <dalibor.jovanov2k1@gmail.com>
2026-03-09 22:24:39 +0100

    updated the local categories to global categories

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/router.jsx";
 
+import { CategoryContextProvider } from "./store/contexts/CategoryContext.jsx";
+
 function App() {
 
   return (
-    <>
+    <CategoryContextProvider>
       <RouterProvider router={router} />
-    </>
+    </CategoryContextProvider>
   )
 }
 
---
+++ frontend/src/store/contexts/CategoryContext.jsx
@@ -0,0 +1,93 @@
+import { createContext, useReducer } from "react";
+import * as api from "../../api/graphqlClient.js";
+
+const CATEGORIES_QUERY = `
+  query {
+    categories {
+      id
+      name
+    }
+  }
+`;
+
+const CategoryContext = createContext({
+    categories: [],
+    loading: false,
+    errors: null,
+    fetchCategories: () => {},
+});
+
+const initialState = {
+    categories: [],
+    loading: false,
+    errors: null,
+};
+
+function categoryReducer(state, action) {
+    switch(action.type) {
+        case "SET_LOADING":
+            return {
+                ...state,
+                loading: true,
+                errors: null,
+            };
+        case "SET_ERROR" :
+            return{
+                ...state,
+                loading: false,
+                errors: action.payload,
+            };
+        case "SET_CATEGORIES":
+            return {
+                ...state,
+                categories: action.payload,
+                loading: false,
+                errors: null,
+            };
+        default: 
+            return state;
+    }
+};
+
+export function CategoryContextProvider({children}){
+    const [category, dispatchCategoryAction] = useReducer(categoryReducer, initialState);
+
+    const fetchCategories = async() => {
+        dispatchCategoryAction({type: "SET_LOADING"});
+
+        try {
+            const res = await api.graphqlRequest(CATEGORIES_QUERY);
+
+            dispatchCategoryAction({
+                type: "SET_CATEGORIES",
+                payload: res.categories,
+            });
+
+            return {success: true};
+        } catch (e) {
+            const message = 
+            e.response?.data?.message || 
+            "Falied to fetch categories.";
+
+            dispatchCategoryAction({
+                type: "SET_ERROR",
+                payload: message,                
+            });
+
+            return {success: false};
+        }
+    }
+
+    const categoryContext = {
+        ...category,
+        fetchCategories,
+    };
+
+    return (
+        <CategoryContext.Provider value={categoryContext}>
+            {children}
+        </CategoryContext.Provider>
+    );
+}
+
+export default CategoryContext;
