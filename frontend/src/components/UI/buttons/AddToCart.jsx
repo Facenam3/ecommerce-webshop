e5739a85bd928fe837576376303969fc40778e48
Dalibor Jovanovski <dalibor.jovanov2k1@gmail.com>
2026-03-21 18:58:30 +0100

    added validation to the add to cart button

--- frontend/src/components/UI/buttons/AddToCart.jsx
+++ frontend/src/components/UI/buttons/AddToCart.jsx
@@ -4,7 +4,10 @@ export default function AddToCart({onClick, disabled, ...props}){
             {...props}
             onClick={onClick}
             disabled={disabled} 
-            className="w-full px-5 py-6 uppercase text-white bg-green-400 text-xl my-4 cursor-pointer"
+            className={`w-full px-5 py-6 uppercase text-xl my-4 ${
+                disabled ? 'bg-gray-500 text-white cursror-not-allowed opacity-60'
+                : 'bg-green-400 text-white hover:bg-green-500 cursor-pointer'
+            }`}
         >
             add to cart
         </button>
