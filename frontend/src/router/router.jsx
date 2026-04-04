5a7913e445a9128c9619156ffdae34bd45564c31
Dalibor Jovanovski <dalibor.jovanov2k1@gmail.com>
2026-04-04 22:49:37 +0200

    added some updates on frontend

--- frontend/src/router/router.jsx
+++ frontend/src/router/router.jsx
@@ -1,10 +1,10 @@
-import { createBrowserRouter, Navigate } from "react-router-dom";
+import { createHashRouter, Navigate } from "react-router-dom";
 import RootLayout from "../pages/layout/Root.jsx";
 import ProductsPage from "../pages/ProductsPage.jsx";
 import Product from "../pages/Product.jsx";
 import ErrorPage from "../pages/error/Errorpage.jsx";
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
     {
         path: "/",
         element: <RootLayout />,
