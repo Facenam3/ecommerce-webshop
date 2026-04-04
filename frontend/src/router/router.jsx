f0e00342d7085647aefb49e8ed8bff1ce3296adb
Dalibor Jovanovski <dalibor.jovanov2k1@gmail.com>
2026-04-04 10:23:05 +0200

    added an error page

--- frontend/src/router/router.jsx
+++ frontend/src/router/router.jsx
@@ -2,11 +2,13 @@ import { createBrowserRouter, Navigate } from "react-router-dom";
 import RootLayout from "../pages/layout/Root.jsx";
 import ProductsPage from "../pages/ProductsPage.jsx";
 import Product from "../pages/Product.jsx";
+import ErrorPage from "../pages/error/Errorpage.jsx";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <RootLayout />,
+        errorElement: <ErrorPage />,
         children: [
             {
                 index: true,
