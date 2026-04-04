69f2c2c4d0168cc792f0398d76f15b7ed3aa263d
Dalibor Jovanovski <dalibor.jovanov2k1@gmail.com>
2026-04-04 22:52:42 +0200

    added the frontend connection to the backend

--- backend/public/index.php
+++ backend/public/index.php
@@ -7,6 +7,7 @@ error_reporting(E_ALL);
 $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
 $allowedOrigins = [
     'http://localhost:5173',
+    'https://facenam3.github.io/ecommerce-webshop/'
 ];
 
 if(in_array($origin, $allowedOrigins, true)) {
