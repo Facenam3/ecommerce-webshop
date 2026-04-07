d9b3e5688771bd49b17fa81c20d845ed8413fe00
Dalibor Jovanovski <dalibor.jovanov2k1@gmail.com>
2026-04-07 17:40:49 +0200

    updated graphqlClient

--- frontend/src/api/graphqlClient.js
+++ frontend/src/api/graphqlClient.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
+const apiUrl = import.meta.env.VITE_API_URL;
+
+if(!apiUrl) {
+    throw new Error("VITE_API_URL is missing. Check your environment variables.");
+}
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: apiUrl,
     headers: {'Content-Type': 'application/json'},
 });
 
