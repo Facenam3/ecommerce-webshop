789e7dc68bfbc29ae372a49be39d19bf280938ac
Dalibor Jovanovski <dalibor.jovanov2k1@gmail.com>
2026-06-02 21:34:40 +0200

    fixed product details image cutting edges with object-contain

--- frontend/src/components/UI/product/ProductCarouselImg.jsx
+++ frontend/src/components/UI/product/ProductCarouselImg.jsx
@@ -5,7 +5,7 @@ export default function ProductCarouselItem({image, index}) {
             <img 
                 src={image} 
                 alt={`Product image ${index + 1}`}
-                className="w-full h-full object-cover"
+                className="w-full h-full object-contain"
                 />
         </div>
     );
